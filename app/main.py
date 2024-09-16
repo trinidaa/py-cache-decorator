fcc32500fead83d434a6211c6927f6573b59f532
Viacheslav <rcop888@gmail.com>
2024-09-16 21:02:50 +0200

    Cache decorator+

--- app/main.py
+++ app/main.py
@@ -11,35 +11,11 @@ def cache(func: Callable) -> Callable:
         # Преобразуем результат в кортеж, чтобы избежать ошибки "unhashable type"
         result_tuple = tuple(result) if isinstance(result, list) else result
         if result_tuple in catcher:
-            print(f"Symbol(s) > {result_tuple} < detect duplicate. Stopping cache!")
+            print("Getting from cache")
             return result
         else:
             catcher.add(result_tuple)
-            print(f"Caching result: {result_tuple}.")
+            print("Calculating new result")
             return result
+
     return wrapper
-
-
-@cache
-def long_time_func(a: int, b: int, c: int) -> int:
-    return (a ** b ** c) % (a * c)
-
-
-@cache
-def long_time_func_2(n_tuple: tuple, power: int) -> list[Any]:
-    return [number ** power for number in n_tuple]
-
-
-long_time_func(1, 2, 3)
-long_time_func(2, 2, 3)
-long_time_func_2((5, 6, 7), 5)
-long_time_func(1, 2, 3)
-long_time_func_2((5, 6, 7), 10)
-long_time_func_2((5, 6, 7), 10)
-
-# Calculating new result
-# Calculating new result
-# Calculating new result
-# Getting from cache
-# Calculating new result
-# Getting from cache
