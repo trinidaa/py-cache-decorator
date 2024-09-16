336fde8dbe05940aed5f97e4541df476a6060f51
Viacheslav <rcop888@gmail.com>
2024-09-16 16:08:52 +0200

    Cache decorator

--- app/main.py
+++ app/main.py
@@ -1,6 +1,44 @@
-from typing import Callable
+from typing import Callable, List, Any
+from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    catcher = set()
+
+    @wraps(func)
+    def wrapper(*args: Callable) -> Callable:
+        result = func(*args)
+        result_tuple = tuple(result) if isinstance(result, list) else result
+        if result_tuple in catcher:
+            print(f"Symbol(s) > {result_tuple} < detect duplicate. Stopping cache! ")
+            return result
+        else:
+            catcher.add(result_tuple)
+            print(f"Caching result: {result_tuple}.")
+            return result
+    return wrapper
+
+
+@cache
+def long_time_func(a: int, b: int, c: int) -> int:
+    return (a ** b ** c) % (a * c)
+
+
+@cache
+def long_time_func_2(n_tuple: tuple, power: int) -> list[Any]:
+    return [number ** power for number in n_tuple]
+
+
+long_time_func(1, 2, 3)
+long_time_func(2, 2, 3)
+long_time_func_2((5, 6, 7), 5)
+long_time_func(1, 2, 3)
+long_time_func_2((5, 6, 7), 10)
+long_time_func_2((5, 6, 7), 10)
+
+# Calculating new result
+# Calculating new result
+# Calculating new result
+# Getting from cache
+# Calculating new result
+# Getting from cache
